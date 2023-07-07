f0e343eb026481b8f2db635e70f1a11fb807846a
Harry <harry@singsnap.com>
2023-07-07 15:34:59 -0400

    fix: animations and various other fixes

--- index.js
+++ index.js
@@ -1,31 +1,37 @@
-// let zosiaElement = document.getElementById("about")
-let options = {
-    threshold: 0.8,
-  };
-// function zosiaCallback(e) {
-//     console.log(zosiaElement.style.right)
-//     if(zosiaElement.style.right == "-100vw") {
-//         zosiaElement.style.right = "0"
-//     }
-// }
+let gandhiTwoElement = document.getElementById("gandhi2")
+let gandhiElement = document.getElementById("gandhi-text")
+let oldScrollY = window.scrollY
+let regExp = /\(([^)]+)\)/;
 
-// let zosiaObserver = new IntersectionObserver(zosiaCallback, options);
-// zosiaObserver.observe(zosiaElement);
-
-// let introElement = document.getElementById("intro")
-// function introCallback(e) {
-//     if(introElement.style.marginLeft == "-100vw") {
-//         introElement.style.marginLeft == "0"
-//     }
-// }
-// let introObserver = new IntersectionObserver(introCallback, options);
-// introObserver.observe(introElement);
-
-let introElement = document.getElementById("intro")
-function introCallback(e) {
-    if(introElement.style.left == "-100vw") {
-        introElement.style.left = "0"
+window.addEventListener('scroll', () => {
+  if(elementIsVisibleInViewport(gandhiTwoElement)) {
+    let scaleValue = parseFloat(regExp.exec(gandhiElement.style.transform)[1])
+    console.log(scaleValue)
+    if(isScrollingDown()) {
+      if(scaleValue <= 1.7) {
+        gandhiElement.style.transform = `scale(${scaleValue + 0.003})`
+      }
+    } else {
+      if(scaleValue >= 1) {
+        gandhiElement.style.transform = `scale(${scaleValue - 0.003})`
+      }
     }
+  }
+});
+function elementIsVisibleInViewport (el) {
+  const rect = el.getBoundingClientRect();
+  return rect.top >= 0 &&
+          rect.left >= 0 &&
+          rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+          rect.right <= (window.innerWidth || document.documentElement.clientWidth);
 }
-let introObserver = new IntersectionObserver(introCallback, options);
-introObserver.observe(document.getElementById("intro_bg"));
+function isScrollingDown() {
+  let result
+  if(oldScrollY < window.scrollY){
+    result = true;
+  } else {
+    result = false;
+  }
+  oldScrollY = window.scrollY;
+  return result
+}
