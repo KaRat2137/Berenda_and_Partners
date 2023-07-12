20ea789c24515b5cdeecc30b353df446b54ca04f
Harry <harry@singsnap.com>
2023-07-12 11:38:40 -0400

    fix: inline script

--- redirect.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load", (event) => {
-    let userLang = navigator.language || navigator.userLanguage;
-    if (userLang == "pl") {
-        window.location.href = "/pl/index.html"
-    }
-    else {
-        window.location.href = "/en/index.html"
-    }
-});
